df9a5ebb0f8ca3caa304fa55719d7c6fcf045d49
K4mp47 <campagnoloalberto5@gmail.com>
2023-06-20 15:14:17 +0200

    added two first users, make the interpreter work with db

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -6,6 +6,14 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db) 
+
+# this code come from stack and chatgpt, for make the 'context'
+# it's actually works and i use it for made function
+# db.session.add() and db.session.commit() works with all the other stuff 
+# for db. If you remove, the python interpreter gives you a RuntimeError out of context.
+
+app.app_context().push()
+
 
 from app import routes, models
